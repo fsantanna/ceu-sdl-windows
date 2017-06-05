e7b51fc3c4b3f9fb32c53a18a841bdb8c3aefb62
Francisco Sant'Anna <francisco.santanna@gmail.com>
2017-06-05 17:51:19 -0300

    (>) make checks file or directory

--- run/make.bat
+++ run/make.bat
@@ -1,20 +1,24 @@
-@set CEU_SRC=%1
-@rem set CEU_SRC=%sfxpar%
-@
-@rem set PATH=c:\mingw\bin
-@set PATH=%~dp0\..\mingw\bin
-@
-@cd %~dp0
-@
-lua53.exe ceu.lua --pre --pre-args="-Iceu\include" --pre-input=%CEU_SRC%\main.ceu --ceu --ceu-line-directives=false --ceu-err-unused=pass --ceu-err-uninitialized=pass --env --env-types=ceu\env\types.h --env-main=ceu\env\main.c --cc --cc-args="-Ic\include -Lc\lib -lm -lmingw32 -lSDL2main -lSDL2 -lSDL2_image" --cc-output=dist\tmp.exe
-@if ERRORLEVEL 1 goto ERROR
-@
-@copy dist\*.* %CEU_SRC%\
-@cd %CEU_SRC%\
-@
+@echo off
+
+set CEU_SRC=%1
+if exist %CEU_SRC%\main.ceu set CEU_SRC=%CEU_SRC%\main.ceu
+for %%A in ("%CEU_SRC%") do (
+    set CEU_DIR=%%~dpA
+)
+
+set PATH=%~dp0\..\mingw\bin
+
+cd %~dp0
+
+lua53.exe ceu.lua --pre --pre-args="-Iceu\include" --pre-input=%CEU_SRC% --ceu --ceu-line-directives=false --ceu-err-unused=pass --ceu-err-uninitialized=pass --env --env-types=ceu\env\types.h --env-main=ceu\env\main.c --cc --cc-args="-Ic\include -Lc\lib -lm -lmingw32 -lSDL2main -lSDL2 -lSDL2_image" --cc-output=dist\tmp.exe
+if ERRORLEVEL 1 goto ERROR
+
+copy dist\*.* %CEU_DIR%\
+cd %CEU_DIR%\
+
 tmp.exe
-@if ERRORLEVEL 1 goto ERROR
-@exit
-@
+if ERRORLEVEL 1 goto ERROR
+exit
+
 :ERROR
-@pause
+pause
