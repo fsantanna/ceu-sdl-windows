2b6d0ebca43a542ce8b06460e2d5b79ddee90902
Francisco Sant'Anna <francisco.santanna@gmail.com>
2017-06-05 16:04:01 -0300

    (>) small adjust

--- make.bat
+++ make.bat
@@ -1,6 +1,9 @@
 set CEU_SRC=%1
+rem set CEU_SRC=%sfxpar%
+
 rem set PATH=c:\mingw\bin
 set PATH=%~dp0\mingw\bin
+
 cd %~dp0
 lua53.exe ceu.lua --pre --pre-args="-Iceu\include" --pre-input=%CEU_SRC%\main.ceu --ceu --ceu-line-directives=false --ceu-err-unused=pass --ceu-err-uninitialized=pass --env --env-types=ceu\env\types.h --env-main=ceu\env\main.c --cc --cc-args="-Ic\include -Lc\lib -lm -lmingw32 -lSDL2main -lSDL2 -lSDL2_image" --cc-output=dist\tmp.exe
 copy dist\*.* %CEU_SRC%\
