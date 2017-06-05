a09bd7e72a158ff4c076a27d85ea6534fe537d54
Francisco Sant'Anna <francisco.santanna@gmail.com>
2017-06-05 19:36:05 -0300

    (+) SDL2_ttf

--- ceu-sdl-windows/run/make.bat
+++ ceu-sdl-windows/run/make.bat
@@ -10,7 +10,7 @@ set PATH=%~dp0\..\mingw\bin
 
 cd %~dp0
 
-lua53.exe ceu.lua --pre --pre-args="-Iceu\include" --pre-input=%CEU_SRC% --ceu --ceu-line-directives=false --ceu-err-unused=pass --ceu-err-uninitialized=pass --env --env-types=ceu\env\types.h --env-main=ceu\env\main.c --cc --cc-args="-Ic\include -Lc\lib -lm -lmingw32 -lSDL2main -lSDL2 -lSDL2_image" --cc-output=dist\tmp.exe
+lua53.exe ceu.lua --pre --pre-args="-Iceu\include" --pre-input=%CEU_SRC% --ceu --ceu-line-directives=false --ceu-err-unused=pass --ceu-err-uninitialized=pass --env --env-types=ceu\env\types.h --env-main=ceu\env\main.c --cc --cc-args="-Ic\include -Lc\lib -lm -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_mixer -lSDL2_ttf" --cc-output=dist\tmp.exe
 if ERRORLEVEL 1 goto ERROR
 
 copy dist\*.* %CEU_DIR%\
