719d4dd7f7f2c3b4100a2e1f4efc33cd25452b6c
Francisco Sant'Anna <francisco.santanna@gmail.com>
2017-06-06 07:07:09 -0300

    (>) path to resources (>) silent copy

--- ceu-sdl-windows/run/make.bat
+++ ceu-sdl-windows/run/make.bat
@@ -13,7 +13,7 @@ cd %~dp0
 lua53.exe ceu.lua --pre --pre-args="-Iceu\include" --pre-input=%CEU_SRC% --ceu --ceu-line-directives=false --ceu-err-unused=pass --ceu-err-uninitialized=pass --env --env-types=ceu\env\types.h --env-main=ceu\env\main.c --cc --cc-args="-Ic\include -Lc\lib -lm -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_mixer -lSDL2_ttf" --cc-output=dist\tmp.exe
 if ERRORLEVEL 1 goto ERROR
 
-copy dist\*.* %CEU_DIR%\
+copy dist\*.* %CEU_DIR%\ > NUL
 cd %CEU_DIR%\
 
 tmp.exe
